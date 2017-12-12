e99f51295499a98aea0017a54e5f27317a681425
Siddharth <siddharth_1004@live.com>
2017-12-12 22:58:20 +0530

    initialized repo

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,30 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log("Unable to connect to MongoDB");
+    }
+    console.log("Connected to MongoDB");
+    /* db.collection('Todos').insertOne({
+        text: 'Something to do',
+        completed: false
+    }, (err, result) => {
+        if (err) {
+            return console.log("Unable to insert todo", err);
+        }
+
+        console.log(JSON.stringify(result.ops, "", 2));
+    }); */
+    /* db.collection('Users').insertOne({
+        name: 'Sidd',
+        age: 23,
+        location: 'Bangalore'
+    }, (err, result) => {
+        if (err) {
+            return console.log("Unable to insert user", err);
+        }
+
+        console.log(result.ops[0]._id.getTimestamp());
+    }); */
+    db.close();
+});
---
+++ playground/mongodb-find.js
@@ -0,0 +1,29 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log("Unable to connect to MongoDB");
+    }
+    console.log("Connected to MongoDB");
+
+    /* db.collection('Todos').find({ completed: true }).toArray().then((docs) => {
+        console.log("Todos");
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log("Unable to fetch Todos", err);
+    }) */
+
+    /*  db.collection('Todos').find().count().then((count) => {
+         console.log(`Todos count: ${count}`);
+     }, (err) => {
+         console.log("Unable to fetch Todos", err);
+     }); */
+
+    db.collection('Users').find({ name: "Sidd" }).count().then((count) => {
+        console.log(`Users count: ${count}`);
+    }, (err) => {
+        console.log("Unable to fetch Users", err);
+    });
+
+    // db.close();
+});
