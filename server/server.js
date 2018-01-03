b794d9418420faec1f7d46df636e7222df8482e0
Siddharth <siddharth_1004@live.com>
2018-01-03 18:41:22 +0530

    Added PATCH and DELETE test

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
@@ -53,11 +54,30 @@ app.delete('/todo/:id', (req, res) => {
     }).catch(() => res.status(400).send());
 });
 
+app.patch('/todo/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+    if (!ObjectID.isValid(req.params.id))
+        return res.status(404).send();
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+
+    }
+    else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({ todo });
+    }).catch((e) => res.status(400).send());
+})
+
 app.listen(port, () => {
     console.log('Started server');
 });
-
-
-
 
 module.exports = { app };
